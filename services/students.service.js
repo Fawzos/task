4ef30dd7698ab87431e95bc2860d33fca23c944f
Fawzos <mohamed.fawzy.elmansoury@gmail.com>
2021-11-11 17:26:56 +0200

    add initialize chapters helper

--- services/students.service.js
+++ services/students.service.js
@@ -1,6 +1,9 @@
+const initializeChapters = require("../utils/generate-chapter-name");
+
 module.exports = {
   calculateStudySessions: ({ startingDate, studyWeekdays, sessions }) => {
     // TODO is to generate the response
-    return { startingDate, studyWeekdays, sessions };
+    const chapters = initializeChapters();
+    return { chapters };
   },
 };
